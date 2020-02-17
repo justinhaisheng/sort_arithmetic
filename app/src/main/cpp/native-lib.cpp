194f069a47498c536b79ef415e0d6802cb4a5d55
haisheng.lu <haisheng.lu@aispeech.com>
2020-02-17 09:43:22 +0800

    nothing

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,7 +6,7 @@
 #include "ArrayUtil.cpp"
 using namespace std;
 
-
+//冒泡排序
 void bubbleSort(int arr[], int len) {
     assert(arr&& len >1);
     for (int i = 0; i < len -1 ; ++i) { //  n-1 步
@@ -50,7 +50,51 @@ void bubbleSortOptimize(int arr[], int len){
     }
 }
 
+// 选择：思想 遍历找出最小的位置，最后与第一个位置进行交换 O(1)
+//最小值排序
 
+void selectSort(int arr[], int len) {
+    for (int i = 0; i < len - 1; ++i) {
+        int min_pos = i;//假设每一趟的第一个为最小值
+        for (int j = i+1; j < len; ++j) {
+             if (arr[min_pos] > arr[j]){
+                 min_pos = j;//记录改变后的位置
+             }
+        }
+        if (min_pos!=i){//如果位置发生了改变则交换
+            ArrayUtil::swap_(arr[i], arr[min_pos]);
+        }
+    }
+}
+
+//插入排序 ，倒叙排序
+void simpleInsertSort(int arr[], int len){
+    for (int i = 1; i < len; ++i) {//从第一个数开始
+        for (int j = i; j >0; j--) {//从后往前比较
+            if (arr[j] < arr [j-1]){
+                ArrayUtil::swap_(arr[j], arr[j-1]);
+            }else{
+                break;//则跳出循环9
+            }
+        }
+    }
+}
+
+void insertSort(int arr[], int len){
+    int j;
+    for (int i = 1; i < len; ++i) {
+        int temp = arr[i];//保存i 这个值
+        for (j = i; j > 0  ; j--) {
+            if (arr[j - 1] > temp){
+                arr[j] = arr[j-1];
+            }else{
+                break;
+            }
+        }
+        arr[j] = temp;
+    }
+
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_aispeech_sort_MainActivity_stringFromJNI(
@@ -58,26 +102,45 @@ Java_com_aispeech_sort_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
 
-    int len = 10000;
+    int len = 10;
     int* arr1 = new int[len];
     int* arr2 = new int[len];
-
-    ArrayUtil::create_random_array_(arr1,len,20,1000);
+    int* arr3 = new int[len];
+    int* arr4 = new int[len];
+    int* arr5 = new int[len];
+    ArrayUtil::create_random_array_(arr1,len,1,20);
     ArrayUtil::copy_random_array_(arr1,arr2,len);
+    ArrayUtil::copy_random_array_(arr1,arr3,len);
+    ArrayUtil::copy_random_array_(arr1,arr4,len);
+    ArrayUtil::copy_random_array_(arr1,arr5,len);
+
+    LOGE("insertSort");
+    ArrayUtil::sort_array_time_("insertSort",insertSort,arr5,len);
+
+    LOGE("simpleInsertSort");
+    ArrayUtil::sort_array_time_("simpleInsertSort",simpleInsertSort,arr4,len);
+
    // LOGE("原始数组");
    // ArrayUtil::print_array_(arr1,len);
     LOGE("bubbleSort");
     ArrayUtil::sort_array_time_("bubbleSort",bubbleSort,arr1,len);
    // ArrayUtil::print_array_(arr1,len);
-    LOGE("bubbleSortOptimize");
-    ArrayUtil::sort_array_time_("bubbleSortOptimize",bubbleSortOptimize,arr2,len);
+    //LOGE("bubbleSortOptimize");
+   // ArrayUtil::sort_array_time_("bubbleSortOptimize",bubbleSortOptimize,arr2,len);
    // ArrayUtil::print_array_(arr2,len);
+    //LOGE("selectSort");
+   // ArrayUtil::sort_array_time_("selectSort",selectSort,arr3,len);
+
+
 
 
 
     //bubbleSortOptimize(arr, );
     //print_array(arr,7);
-    delete(arr1);
-    delete(arr2);
+    delete (arr1);
+    delete (arr2);
+    delete (arr3);
+    delete (arr4);
+    delete (arr5);
     return env->NewStringUTF(hello.c_str());
 }
